86e5a679e2ab68c822f698cb5e4ab8fefbafb641
PPdustin <dastennn02@gmail.com>
2023-11-14 15:37:35 +0800

    Loginphp added

---
+++ login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="login.css">
+</head>
+<body>
+    <div class="login_container">
+        <form action="login.php" method="post">
+    
+        <label for="name">username:</label>
+        <input type="text" name="username" required><br><br>
+        
+        <label for="password">password: </label>
+        <input type="text" name="password" required><br><br>
+        
+        <button type="submit">login</button>
+    </div>
+</body>
+</html>
